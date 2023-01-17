880f61ead6cecb4e3c3f2edb6ce64c9f565a4631
Hann0T <hansolivas1@gmail.com>
2023-01-17 11:43:19 -0500

    feat: new api routes.

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\GradeController;
+use App\Http\Controllers\GradePersonalController;
 use App\Http\Controllers\GradeM2MController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PersonalAccessTokenController;
+use App\Http\Middleware\AddScopesByRole;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,12 +23,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::middleware('auth:api')->prefix('grades')->group(function () {
-//     Route::middleware('scopes:view-grades')
-//         ->get('/', [GradeController::class, 'index']);
-//     Route::middleware('scopes:update-grades')
-//         ->post('/update/{grade}', [GradeController::class, 'update']);
-// });
+Route::middleware('auth:api')->prefix('grades')->group(function () {
+    Route::middleware('scopes:view-grades')
+        ->get('/', [GradeController::class, 'index']);
+    Route::middleware('scopes:update-grades')
+        ->post('/update/{grade}', [GradeController::class, 'update']);
+});
 
 Route::prefix('m2m/grades')->group(function () {
     Route::middleware('client:view-grades')
@@ -33,3 +36,13 @@ Route::prefix('m2m/grades')->group(function () {
     Route::middleware('client:update-grades')
         ->post('/update/{grade}', [GradeM2MController::class, 'update']);
 });
+
+Route::middleware('auth:api')->prefix('personal/grades')->group(function () {
+    Route::middleware('client:view-grades')
+        ->get('/', [GradePersonalController::class, 'index']);
+    Route::middleware('client:update-grades')
+        ->post('/update/{grade}', [GradePersonalController::class, 'update']);
+});
+
+Route::middleware(AddScopesByRole::class)
+    ->post('personal/token/issue', [PersonalAccessTokenController::class, 'getToken']);
